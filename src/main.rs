66214f3eccb2e647cdce55e447b86e3d6959771c
lorenzo.filippini <lorenzo.filippini@omniacom.it>
2022-11-13 23:27:44 +0100

    commit5

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::fs;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use lopdf::Document;
+use chrono::Datelike;
 
-static FILENAME: &str = "testFile.txt";
+static FILENAME: &str = "dipendenti.txt";
 static OUTPUTDIR: &str = "output";
+static PDFNAME: &str = "bustepaga.pdf";
 
 fn main() {
     loop {
@@ -42,20 +44,25 @@ fn main() {
         let mut dipendenti = Vec::new();
         read_file(&mut dipendenti);
 
-        println!("Presenti: {} dipendenti", dipendenti.len());
-        
-        for value in &dipendenti {
-            println!("{}", value);
+        let dip_num = dipendenti.len();
+        println!("Presenti: {} dipendenti", dip_num);
+        if dip_num == 0 {
+            println!("Non ci sono dipendenti nel file: {}, aggiungi i nomi per continuare", FILENAME);
+            continue;
         }
+        
+        // for value in &dipendenti {
+        //     println!("{}", value);
+        // }
 
         fs::create_dir_all(OUTPUTDIR).unwrap();
 
-        load_pdf();
+        load_pdf(dipendenti);
     }
 }
 
 fn print_menu() {
-    println!("1) Test");
+    println!("1) Dividi buste paga per dipendente");
     println!("2) Esci");
     println!("Inserici il valore desiderato e premi INVIO");
 }
@@ -67,7 +74,7 @@ fn read_file(dip: &mut Vec<String>) {
 
     let greeting_file = match file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(_) => panic!("Non riesco a trovare il file dipendenti: {}", FILENAME),
     };
 
     let reader = BufReader::new(greeting_file);
@@ -83,26 +90,48 @@ fn read_file(dip: &mut Vec<String>) {
     }
 }
 
-fn load_pdf() {
-    let doc = Document::load("UUU.pdf").unwrap();
-    let count = doc.get_pages().len().try_into().unwrap();
+fn get_date() -> String {
+    let current_date = chrono::Utc::now();
+    let year = current_date.year();
+    let month = current_date.month();
+    return format!("{}{}{}{}", "_", month, "_", year);
+}
+
+fn load_pdf(dip: Vec<String>) {
+    let doc = match Document::load(PDFNAME) {
+        Ok(_doc) => _doc,
+        Err(_) => panic!("Non riesco a trovare il file buste paga: {}", PDFNAME)
+    };
+    let count = doc.get_pages().len();
     println!("Il PDF contiene {} buste paga", count);
+
+    if count > dip.len() {
+        println!("Ci sono piú buste paga che dipendenti!");
+        return;
+    } 
+    if count < dip.len() {
+        println!("Ci sono piú dipendenti che buste paga!");
+        return;
+    }
+
+    let data = get_date();
+
     for n in 1..=count {
         let mut doc_copy = doc.clone();
-        let mut name = String::from("page");
-        name.push_str(&n.to_string());
-        name.push_str(".pdf");
+        let mut file_name = String::from(&dip[n-1]);
+        file_name.push_str(&data);
+        file_name.push_str(".pdf");
 
         let mut del_pages: Vec<u32> = Vec::new();
         for x in 1..=count {
             if x == n {
                 continue;
             }
-            del_pages.push(x);
+            del_pages.push(x.try_into().unwrap());
         }
         doc_copy.delete_pages(&del_pages);
-        let path = format!("{}{}{}", OUTPUTDIR, "/", name);
-        println!("Creazione della busta paga numero {} di {}:", n, name);
+        let path = format!("{}{}{}", OUTPUTDIR, "/", file_name);
+        println!("Creazione della busta paga numero {}, nome: {}", n, file_name);
         doc_copy.save(path).unwrap();
     }
 }
